adc54dad40006cd9790770144d674de992c358b6
las5h4 <loganasmalley@gmail.com>
2020-10-29 16:09:56 -0500

    added SkillsController, created html string that lives at /skills/

---
+++ SkillsTracker/Controllers/SkillsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SkillsTracker.Controllers
+{
+    public class SkillsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            string html = "<h1>" + "Skills Tracker" + "</h1>" +
+                "<h2>" + "Skills to learn:" + "</h2>" +
+                "<ol>" +
+                "<li>" + "C#" + "</li>" +
+                "<li>" + "JavaScript" + "</li>" +
+                "<li>" + "Python" + "</li>" +
+                "</ol>";
+            return Content(html, "text/html");
+        }
+    }
+}
