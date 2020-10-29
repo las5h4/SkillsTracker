aa199f305e28007700013c724edadc5c5751ae81
las5h4 <loganasmalley@gmail.com>
2020-10-29 17:02:07 -0500

    completed form at /skills/form/ and post processing for form

--- SkillsTracker/Controllers/SkillsController.cs
+++ SkillsTracker/Controllers/SkillsController.cs
@@ -22,5 +22,48 @@ namespace SkillsTracker.Controllers
                 "</ol>";
             return Content(html, "text/html");
         }
+
+        public IActionResult Form()
+        {
+            string html = "<form method = 'post' action = '/skills/form/'>" +
+                "<input type='date' label='Date: ' name='date'/>" +
+                "<br>" +
+                "<label for='csharp'>" + "C#:" + "</label>" +
+                "<select name='csharp'>" +
+                "<option value='Learning Basics'>" + "Learning Basics" + "</option>" +
+                "<option value='Making Apps'>" + "Making Apps" + "</option>" +
+                "<option value='Master Coder'>" + "Master Coder" + "</option>" +
+                "</select>" +
+                "<br>" +
+                "<label for='javaScript'>" + "JavaScript:" + "</label>" +
+                "<select name='javaScript'>" +
+                "<option value='Learning Basics'>" + "Learning Basics" + "</option>" +
+                "<option value='Making Apps'>" + "Making Apps" + "</option>" +
+                "<option value='Master Coder'>" + "Master Coder" + "</option>" +
+                "</select>" +
+                "<br>" +
+                "<label for='python'>" + "Python:" + "</label>" +
+                "<select name='python'>" +
+                "<option value='Learning Basics'>" + "Learning Basics" + "</option>" +
+                "<option value='Making Apps'>" + "Making Apps" + "</option>" +
+                "<option value='Master Coder'>" + "Master Coder" + "</option>" +
+                "</select>" +
+                "<br>" +
+                "<input type='submit' value='Submit'/>" +
+                "</form>";
+            return Content(html, "text/html");
+        }
+
+        [HttpPost]
+        public IActionResult Form(string date, string csharp, string javaScript, string python)
+        {
+            string html ="<h1>" + date + "</h1>" +
+                "<ol>" +
+                "<li>" + "C#: " + csharp + "</li>" +
+                "<li>" + "JavaScript: " + javaScript + "</li>" +
+                "<li>" + "Python: " + python + "</li>" +
+                "</ol>";
+            return Content(html, "text/html");
+        }
     }
 }
